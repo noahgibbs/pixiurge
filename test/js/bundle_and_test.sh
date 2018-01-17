a385c7da12e5e6f6a2d695ac3c9525969b5e41e2
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-01-17 14:42:49 -0800

    Use Sinatra server and mocha-headless-chrome for running bundled tests. WIP: Sinatra won't die if the tests fail.

--- test/js/bundle_and_test.sh
+++ test/js/bundle_and_test.sh
@@ -3,9 +3,18 @@
 set -e
 set -x
 
-`which mocha-headless-chrome` || npm install -g mocha-headless-chrome
+which mocha-headless-chrome || npm install -g mocha-headless-chrome
 
 ./node_modules/.bin/webpack --config test/js/webpack.config.js
-#ruby test/js/test_server/server.rb &  # Run the Sinatra server for the tests
+
+pushd test/js/test_server
+rackup -p 6543 &
+sleep 1
+popd
+
 #open -a "Google Chrome.app" test/js/test_server/mocha_tests.html
-mocha-headless-chrome -f test/js/test_server/mocha_tests.html
+#mocha-headless-chrome -f test/js/test_server/mocha_tests.html
+mocha-headless-chrome -f http://localhost:6543/mocha_tests.html
+
+# Now get rid of that Sinatra server
+kill %1
