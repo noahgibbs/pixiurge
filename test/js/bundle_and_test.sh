5e089429efd53a3866665c57c61cdde7a45f8587
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-01-17 12:50:16 -0800

    Switch from opening regular Chrome to testing with mocha-headless-chrome on the command line

--- test/js/bundle_and_test.sh
+++ test/js/bundle_and_test.sh
@@ -3,6 +3,9 @@
 set -e
 set -x
 
+`which mocha-headless-chrome` || npm install -g mocha-headless-chrome
+
 ./node_modules/.bin/webpack --config test/js/webpack.config.js
 #ruby test/js/test_server/server.rb &  # Run the Sinatra server for the tests
-open -a "Google Chrome.app" test/js/test_server/mocha_tests.html
+#open -a "Google Chrome.app" test/js/test_server/mocha_tests.html
+mocha-headless-chrome -f test/js/test_server/mocha_tests.html
