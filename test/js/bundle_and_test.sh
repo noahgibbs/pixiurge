b67c02c0d6efb09fac683772fe239becac24f931
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-01-22 23:38:58 -0800

    Fix JS tests - out-of-order Pixiurge CoffeeScript files were causing total failures

--- test/js/bundle_and_test.sh
+++ test/js/bundle_and_test.sh
@@ -13,11 +13,13 @@ kill `ps | grep "rackup" | grep "6543" | cut -d " " -f 1` || echo "No kill, no p
 pushd test/js/test_server
 rackup -p 6543 &
 sleep 1
+curl http://localhost:6543 || echo "Not up yet..."
 popd
 
 #open -a "Google Chrome.app" test/js/test_server/mocha_tests.html
 #mocha-headless-chrome -f test/js/test_server/mocha_tests.html
 mocha-headless-chrome -f http://localhost:6543/mocha_tests.html
+#open -a "Google Chrome.app" http://localhost:6543/mocha_tests.html
 
 # Now get rid of that test web server
 kill `ps | grep "rackup" | grep "6543" | cut -d " " -f 1` || echo "No kill, no problem."
