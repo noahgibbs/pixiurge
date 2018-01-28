aece5688578a15aa090a7588b868bbbfa8f2c269
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-01-28 11:21:21 -0800

    Significant improvements to generator, using snakecase names and human names

--- lib/pixiurge/commands/generator.rb
+++ lib/pixiurge/commands/generator.rb
@@ -9,14 +9,26 @@ module Pixiurge
 
       argument :name
 
+      def human_name
+        @human_name ||= snakecase_name.split("_").map(&:capitalize).join(" ")
+      end
+
+      def camelcase_name
+        @camel_name ||= snakecase_name.split("_").map(&:capitalize).join
+      end
+
+      def snakecase_name
+        @snake_name ||= name.gsub(" ", "_").gsub(/([a-z])([A-Z])/, "$1_$2")
+      end
+
       def self.source_root
         File.join __dir__, "..", "..", "..", "generator"
       end
 
       #desc "new GAMENAME", "Creates a new Pixiurge game or application"
       def top_level_directory
-        directory "%name%", :mode => :preserve
-        chmod(name + "/exe/start_server", 0755)
+        directory "%snakecase_name%", :mode => :preserve
+        chmod(snakecase_name + "/exe/start_server", 0755)
       end
     end
   end
