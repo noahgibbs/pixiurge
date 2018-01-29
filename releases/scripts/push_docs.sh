1951ebe6b06954d4b001dc110e5a78af991256d6
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-01-29 15:10:23 -0800

    Filter out minimally-changed HTML from the push_docs script

--- releases/scripts/push_docs.sh
+++ releases/scripts/push_docs.sh
@@ -11,10 +11,13 @@ rm -rf .yardoc doc
 yardoc
 
 cd ../pixiurge
-git rm -r docs/pixiurge
-git rm -r docs/demiurge
+git rm -rf docs/pixiurge
+git rm -rf docs/demiurge
 cp -r ../demiurge/doc docs/demiurge
 cp -r doc docs/pixiurge
 git add docs/demiurge docs/pixiurge
+
+# Everything has a "generated at <time>" line. We don't want files that change only that line.
+git diff --cached --numstat | grep "\<1\t1\t" | cut -f 3 | xargs git reset
 
 echo This is where a 'git push' might be helpful.
